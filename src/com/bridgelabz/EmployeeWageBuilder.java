d1668e508606619a4c091ff372557ed67e2a686b
Sakshi <sakshishetty1231997@gmail.com>
2021-08-03 16:57:47 +0530

    Added: Welcome Message to the Program

---
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+/**
+ * @author Sakshi Shetty
+ * Purpose - Employee Wage Computation Program
+ */
+public class EmployeeWageBuilder {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Program");
+	}
+}
