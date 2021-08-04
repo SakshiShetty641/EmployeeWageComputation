c298d65042334cab2f17efc63f589a323bc02d3f
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 14:44:34 +0530

    Added:To Declare a part time employee and calculate daily wage

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,27 +1,30 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - To calculate the daily wage of an Employee
+ * Purpose - To declare a part time employee and calculate the daily wage of an Employee
  */
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Program");
 		// Declaring a constant
 		int IS_FULLTIME = 1;
+		int IS_PARTTIME = 2;
 		int Emp_Rate_PerHr = 20;
-        // Declaring a variables
+		// Declaring a variables
 		int empHrs = 0;
 		int empWage = 0;
-        /**
+		/**
 		 * Using Math.random method to generate random numbers 0, 1 and 2
 		 */
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULLTIME)
 			empHrs = 8;
+		else if (empCheck == IS_PARTTIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		/**
-		 * Calculating the daily employee wage assuming wage per hour as 20 and Full day hour as 8
+		 * Calculating the daily employee wage of an employee
 		 */
 		empWage = Emp_Rate_PerHr * empHrs;
 		System.out.println("The daily wage of an employee is:-" + empWage);
@@ -29,4 +32,3 @@ public class EmployeeWageBuilder {
 	}
 
 }
-
