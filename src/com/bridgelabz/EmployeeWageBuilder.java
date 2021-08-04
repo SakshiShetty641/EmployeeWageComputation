db5ed37ad3d69802b209d2e975e8ae9c6e7de06b
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 14:34:09 +0530

    Added: To calculate the daily wage of an Employee

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,30 +1,32 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - To calculate the daily Employee Wage
+ * Purpose - To calculate the daily wage of an Employee
  */
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Program");
 		// Declaring a constant
-		int IS_PRESENT = 1;
-		int Emp_Wage = 0;
-		int Emp_Hrs = 0;
+		int IS_FULLTIME = 1;
 		int Emp_Rate_PerHr = 20;
-		/**
-		 * Using Math.random method to generate random numbers 0 and 1
+        // Declaring a variables
+		int empHrs = 0;
+		int empWage = 0;
+        /**
+		 * Using Math.random method to generate random numbers 0, 1 and 2
 		 */
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_PRESENT) {
-			/**
-			 * Calculating the daily employee wage assuming wage per hour as 20 and Full day hour as 8
-			 */
-			Emp_Hrs = 8;
-			Emp_Wage = Emp_Rate_PerHr * Emp_Hrs;
-			System.out.println("The employee is present and the daily wage of an employee is:-" + Emp_Wage);
-		} else {
-			System.out.println("The Employee is Absent and the daily employee wage is:-" + 0);
-		}
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULLTIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		/**
+		 * Calculating the daily employee wage assuming wage per hour as 20 and Full day hour as 8
+		 */
+		empWage = Emp_Rate_PerHr * empHrs;
+		System.out.println("The daily wage of an employee is:-" + empWage);
 
 	}
+
 }
+
